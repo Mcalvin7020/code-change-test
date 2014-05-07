47428ad9e28ac972e61d068d60e8b8828e6d10de
Mcalvin7020 <mcalvin7020@sohamcollege.org.uk>
2014-05-07 09:35:22 +0100

    Update greenBottles.py
    I've added a loop to shorten the lines of codes

--- greenBottles.py
+++ greenBottles.py
@@ -1,4 +1,7 @@
-print("10 green bottles, hanging on the wall")
-print("10 green bottles, hanging on the wall")
-print("And if 1 green bottle should acidentally fall,")
-print("They'll be 9 green bottles hanging on the wall.")
+bottles=10
+while bottles !=0:
+  print("{0} green bottles sitting on a wall,".format(bottles))
+  print("{0} green bottles sitting on a wall,".format(bottles))
+  print("and if one green bottle should accidently fall,")
+  bottles=bottles-1
+  print("there'll {0} green bottles sitting on the wall.")
